5bb8f8fcff21e9bc4476c4802ed76eb5725d1c78
Matt Gardner <mjg82@byu.edu>
2011-05-31 06:02:31 -0600

    Added some stuff for unicode, updated formatting slightly

--- remove_html_tags.py
+++ remove_html_tags.py
@@ -1,11 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+
 def remove_html_tags(line, htmlsequences):
     newline = line
     for sequence in htmlsequences:
         newline = newline.replace(sequence, htmlsequences[sequence])
     return newline
+
+
+def unicode_to_ascii(line, conversions):
+    newline = line
+    for sequence in conversions:
+        newline = newline.replace(sequence, conversions[sequence])
+    return newline
+
+
+def unicode_to_latex(line, conversions):
+    # This assumes the same conversion dictionary as in unicode_to_ascii,
+    # so we make a few small modifications before proceeding
+    conversions[u'“'] = '``'
+    conversions[u'”'] = "''"
+    conversions[u'‘'] = "`"
+    conversions[u'’'] = "'"
+    conversions[u'…'] = '\ldots '
+    conversions['...'] = '\ldots '
+    conversions['. . .'] = '\ldots '
+    conversions[u'–'] = '---'
+    conversions[u'#'] = '\#'
+    newline = line
+    for sequence in conversions:
+        newline = newline.replace(sequence, conversions[sequence])
+    return newline
+
 
 def remove_comments(line):
     import re
@@ -17,11 +44,13 @@ def remove_comments(line):
         match = r.match(newline)
     return newline
 
+
 def has_html_tags(line, htmlsequences):
     for sequence in htmlsequences:
         if sequence in line:
             return True
     return False
+
 
 def get_html_sequences():
     htmlsequences = dict()
@@ -66,6 +95,7 @@ def get_html_sequences():
     htmlsequences['&lsqb;'] = u'['
     htmlsequences['&rsqb;'] = u']'
     htmlsequences['&mdash;'] = u'\u2014'
+    htmlsequences['&shy;'] = u'\u2013'
     htmlsequences['&breve;'] = u'\u0306'
     htmlsequences['&dagger;'] = u'\u2020'
     htmlsequences['&macr;'] = u'\u0304'
@@ -78,18 +108,17 @@ def get_html_sequences():
     htmlsequences['&verbar;'] = u'|'
     # I don't know what these sequences are, but they are here so I can ignore
     # them in another program I'm writing
-    htmlsequences['&open;'] = u''
-    htmlsequences['&close;'] = u''
-    htmlsequences['&shy;'] = u''
-    htmlsequences['&top;'] = u''
-    htmlsequences['&dot;'] = u''
-    htmlsequences['&ldsqb;'] = u''
-    htmlsequences['&rdsqb;'] = u''
-    htmlsequences['&lang;'] = u''
-    htmlsequences['&num;'] = u''
-    htmlsequences['&rpress;'] = u''
-    htmlsequences['&lpress;'] = u''
-    htmlsequences['&stigma;'] = u''
+    #htmlsequences['&open;'] = u''
+    #htmlsequences['&close;'] = u''
+    #htmlsequences['&top;'] = u''
+    #htmlsequences['&dot;'] = u''
+    #htmlsequences['&ldsqb;'] = u''
+    #htmlsequences['&rdsqb;'] = u''
+    #htmlsequences['&lang;'] = u''
+    #htmlsequences['&num;'] = u''
+    #htmlsequences['&rpress;'] = u''
+    #htmlsequences['&lpress;'] = u''
+    #htmlsequences['&stigma;'] = u''
 
     htmlsequences['&#039;'] = u"'"
     htmlsequences['&#91;'] = u'['
@@ -113,5 +142,62 @@ def get_html_sequences():
     htmlsequences['&#250;'] = u'ú'
 
     return htmlsequences
+
+
+def get_unicode_conversions():
+    conversions = dict()
+    conversions[u'Å'] = 'A'
+    conversions[u'á'] = 'a'
+    conversions[u'à'] = 'a'
+    conversions[u'ã'] = 'a'
+    conversions[u'â'] = 'a'
+    conversions[u'ä'] = 'a'
+    conversions[u'æ'] = 'ae'
+    conversions[u'é'] = 'e'
+    conversions[u'è'] = 'e'
+    conversions[u'í'] = 'i'
+    conversions[u'ï'] = 'i'
+    conversions[u'ó'] = 'o'
+    conversions[u'ô'] = 'o'
+    conversions[u'ö'] = 'o'
+    conversions[u'ø'] = 'o'
+    conversions[u'ú'] = 'u'
+    conversions[u'ù'] = 'u'
+    conversions[u'ü'] = 'u'
+    conversions[u'ç'] = 'c'
+    conversions[u'ñ'] = 'n'
+    conversions[u'“'] = '"'
+    conversions[u'”'] = '"'
+    conversions[u'‘'] = "'"
+    conversions[u'’'] = "'"
+    conversions[u'–'] = '-'
+    conversions[u'—'] = '-'
+    conversions[u'¿'] = ''
+    conversions[u'…'] = '...'
+    conversions[u'•'] = ''
+    conversions[u'£'] = ''
+    conversions[u'°'] = ''
+    conversions[u'±'] = ''
+    conversions[u'©'] = ''
+    conversions[u'®'] = ''
+    conversions[u'¢'] = ''
+    conversions[u'™'] = ' TM '
+    conversions[u'\ufb01'] = 'fi'
+    conversions[u'\xde'] = 'fi'
+    conversions[u'\ufb02'] = 'fl'
+    conversions[u'\xdf'] = 'fl'
+    # This is a bullet point, or is supposed to be
+    conversions[u'\xa5'] = ' - '
+    conversions[u'\u2014'] = '-'
+    conversions[u'\u0306'] = ''
+    conversions[u'\u2020'] = ''
+    conversions[u'\u2029'] = '\n\n'
+    conversions[u'\u0304'] = ''
+    conversions[u'\u230f'] = ''
+    conversions[u'\u230e'] = ''
+    conversions[u'\xa0'] = ' '
+    conversions[u'\xca'] = ' '
+    return conversions
+
 
 # vim: et sw=4 sts=4
